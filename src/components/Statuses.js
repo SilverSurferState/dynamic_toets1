2382352c26323fcb98b5e756a2de2a217e4bdca2
beto <beto@asuvorm.be>
2022-05-16 19:07:00 +0200

    Vraag 4

--- src/components/Statuses.js
+++ src/components/Statuses.js
@@ -2,22 +2,38 @@
 import {Section} from "./Section";
 import {MyCard} from "./MyCard";
 import PropTypes from "prop-types";
+import {Col} from "react-bootstrap";
 
 
 export function Status(props){
     const {status} = props;
     console.log(status)
     return <>
-        <Section>
+        <Col xs={12} sm={6} md={4} lg={3} xl={2}>
             <MyCard title={status.description}>
                 {status.amount}
             </MyCard>
-        </Section>
+        </Col>
     </>
 
+}
+
+export function Statuses(props){
+    const {statuses, title} = props;
+    return <>
+        <Section title={title}>
+            {statuses.map((status, index) => <Status key={index} status={status} ></Status>)}
+        </Section>
+
+    </>
 }
 
 Status.propTypes = {
     description: PropTypes.string,
     amount : PropTypes.number
+}
+
+Statuses.propTypes = {
+    statuses: PropTypes.arrayOf(PropTypes.object).isRequired,
+    title: PropTypes.string
 }
--- src/pages/CarsPage.js
+++ src/pages/CarsPage.js
@@ -1,14 +1,17 @@
 import {Cars} from "../components/Cars";
-import {Status} from "../components/Statuses";
+import {Status, Statuses} from "../components/Statuses";
+import {Section} from "../components/Section";
 
 export function CarsPage(props) {
     const {cars} = props
     const status = {description: "new arrival", "amount": 5}
+    const statuses = [{description: "new arrival", "amount": 5}, {description: "for sale", "amount": 3},{description: "sold", "amount": 1}]
     return (
         <div className="mx-3" >
             <Cars cars={cars} title="Auto's"/>
             <Cars cars={cars.filter(car => car.status !== "sold")} title="Auto's TE KOOP"/>
-            <Status status={status}></Status>
+            <Section title="Test Status"><Status status={status}></Status></Section>
+            <Statuses statuses={statuses} title="Test Statuses"></Statuses>
         </div>
     );
 }
